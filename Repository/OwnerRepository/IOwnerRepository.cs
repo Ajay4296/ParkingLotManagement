98f7f408e5c8dcdd0bea29d68c4f7feb1e32cb42
Ajay <ajayk4296@gmail.com>
2020-05-02 11:43:58 +0530

    [Ajay]Add. implementation of parking lot problem

--- Repository/OwnerRepository/IOwnerRepository.cs
+++ Repository/OwnerRepository/IOwnerRepository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Repository.OwnerRepository
 {
    public  interface IOwnerRepository
     {
+        Task<int> AddParking();
+        ParkingModel GetVehicle();
+        ParkingModel RemoveVehicle();
     }
 }
