95a250c64a35226359187c337b43d4df5f1696ca
Ajay <ajayk4296@gmail.com>
2020-05-02 17:38:04 +0530

    [Ajay]Add. implementation of parking lot problem

--- Manager/PoliceManager.cs
+++ Manager/PoliceManager.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Model;
+using Repository.policeRepository;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Manager
 {
-   public class PoliceManager
+   public class PoliceManager:IPoliceManager
     {
+        private readonly IPoliceRepository policeRepository;
+
+        public PoliceManager(IPoliceRepository Repository)
+        {
+            this.policeRepository = policeRepository;
+        }
+        public ParkingModel GetVehicle(int slotNumber)
+        {
+            return this.policeRepository.GetVehicle(slotNumber);
+        }
+        public Task<int> AddParking(ParkingModel parking)
+        {
+            return this.policeRepository.AddParking(parking);
+        }
+        public ParkingModel UnParking(int parkingSlotId)
+        {
+            return this.policeRepository.UnPark(parkingSlotId);
+        }
     }
 }
--- Repository/policeRepository/PoliceRepository.cs
+++ Repository/policeRepository/PoliceRepository.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Repository.policeRepository
 {
-   public interface IPoliceRepository
+   public interface PoliceRepository
     {
     }
 }
