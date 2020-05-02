e99ce49948eedf0b36e492a2f0b657a4d55333de
Ajay <ajayk4296@gmail.com>
2020-05-02 17:48:34 +0530

    [Ajay]Add. implementation of parking lot problem

--- Manager/SecurityManager.cs
+++ Manager/SecurityManager.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Model;
+using Repository.SecurityRepository;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Manager
 {
-     public class SecurityManager
+     public class SecurityManager:ISecurityManager
     {
+        private readonly ISecurityRepository securityRepository;
+
+        public SecurityManager(ISecurityRepository ownerRepository)
+        {
+            this.securityRepository = securityRepository;
+        }
+        public ParkingModel GetVehicle(int slotNumber)
+        {
+            return this.securityRepository.GetVehicle(slotNumber);
+        }
+        public Task<int> AddParking(ParkingModel parking)
+        {
+            return this.securityRepository.AddParking(parking);
+        }
+        public ParkingModel UnParking(int parkingSlotId)
+        {
+            return this.securityRepository.UnPark(parkingSlotId);
+        }
     }
 }
