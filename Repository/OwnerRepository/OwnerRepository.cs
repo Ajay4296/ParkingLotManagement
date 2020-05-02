e496ee1cde6a0ddea79eaf94ce2540ff0d31d03e
Ajay <ajayk4296@gmail.com>
2020-05-02 12:06:36 +0530

    [Ajay]Add. implementation of parking lot problem

--- Repository/OwnerRepository/OwnerRepository.cs
+++ Repository/OwnerRepository/OwnerRepository.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Model;
+using Repository.UserDBContext;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Repository.OwnerRepository
 {
-   public class OwnerRepository
+   public class OwnerRepository : IOwnerRepository
     {
+        private readonly UserDbContext userContext;
+        public OwnerRepository(UserDbContext userContext)
+        {
+            this.userContext = userContext;
+        }
+        public ParkingModel GetVehicle(int slotNumber)
+        {
+            return userContext.ParkingSpace.Find(slotNumber);
+        }
+        public ParkingModel AddParking()
     }
 }
